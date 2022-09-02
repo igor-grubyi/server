d5078546e5cae0495170ef1d16d2070262314e3c
Igor Grubyi <grubiy2011@gmail.com>
2022-09-02 15:26:56 +0300

    first commit

---
+++ resolvers.js
@@ -0,0 +1,45 @@
+import { Company, Job, User } from './db.js';
+
+const rejectIf = (condition) => {
+  if (condition) throw new Error('Not autorized');
+};
+
+export const resolvers = {
+  Query: {
+    company: (_root, { id }) => Company.findById(id),
+    job: (_root, { id }) => Job.findById(id),
+    jobs: () => Job.findAll(),
+  },
+
+  Mutation: {
+    createJob: async (_root, { input }, { user }) => {  
+      rejectIf(!user); 
+  
+      return Job.create({ ...input, companyId: user.companyId });
+    },
+    deleteJob: async (_root, { id }, { user }) => {
+      rejectIf(!user);
+      const job = await Job.findById(id);
+      rejectIf(job?.companyId !== user.companyId);
+
+      return Job.delete(id);
+    },
+    updateJob: async (_root, { input }, { user }) => {
+      rejectIf(!user);
+      console.log('user exist');
+      const job = await Job.findById(input.id);
+      console.log('job: ', job);
+      rejectIf(job?.companyId !== user.companyId);
+
+      return Job.update({ ...input, companyId: user.companyId });
+    },
+  },
+
+  Company: {
+    jobs: (company) => Job.findAll(job => job.companyId === company.id), 
+  },
+
+  Job: {
+    company: (job) => Company.findById(job.companyId),
+  },
+}
